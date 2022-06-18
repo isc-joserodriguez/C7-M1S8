864cf98e94c430193136a321a037c8c08577e774
Jose Rodriguez <isc.joserodriguez@gmail.com>
2022-06-18 11:56:27 -0600

    Ejemplo | Agregar y editar usuario

--- main.js
+++ main.js
@@ -30,3 +30,82 @@ function miFuncion(event) {
 }
 
 miBoton.addEventListener('click', miFuncion);
+
+
+const contactoForm = document.getElementById('contacto-form');
+const inputNombre = document.getElementById('inputNombre');
+const inputApellido = document.getElementById('inputApellido');
+const inputTelefono = document.getElementById('inputTelefono');
+const bodyTabla = document.getElementById('body-tabla');
+const contactos = [{
+    nombre: 'Maria',
+    apellido: 'Lopez',
+    telefono: '1256432184',
+},
+{
+    nombre: 'Maria',
+    apellido: 'Lopez',
+    telefono: '9923432184',
+},
+];
+
+
+function agregarContacto(nombre, apellido, telefono) {
+    contactos.push({
+        nombre,
+        apellido: apellido,
+        telefono: telefono,
+    })
+}
+
+function eliminarContacto(indice) {
+
+    contactos.splice(indice, 1)
+}
+
+function mostrarContactos() {
+    bodyTabla.innerHTML = '';
+    contactos.forEach(function (contacto, indice) {
+        bodyTabla.innerHTML += `<tr>
+        <th scope="row">${indice + 1}</th>
+        <td>${contacto.nombre}</td>
+        <td>${contacto.apellido}</td>
+        <td>${contacto.telefono}</td>
+        <td>
+        <button class="btn btn-warning" onclick="editarContacto(${indice})">editar</button>
+        <button class="btn btn-danger">eliminar</button>        
+        </td>
+        </tr>`
+    })
+}
+
+function editarContacto(indice) {
+    contactos[indice].nombre = prompt('Ingresa un nuevo nombre.');
+    contactos[indice].apellido = prompt('Ingresa un nuevo apellido.');
+    contactos[indice].telefono = prompt('Ingresa un nuevo telefono.');
+
+    mostrarContactos();
+}
+
+
+contactoForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    /* 
+    numero1 = numero1 + 123424123;
+    numero1 += 123424123;
+     */
+    if (inputNombre.value.trim() !== '' && inputApellido.value.trim() !== '' && inputTelefono.value.trim() !== '') {
+
+        bodyTabla.innerHTML = '';
+
+        agregarContacto(inputNombre.value, inputApellido.value, inputTelefono.value);
+
+        mostrarContactos();
+
+        event.target.reset();
+    } else {
+        alert('Los 3 campos son obligatorios');
+    }
+});
+
+mostrarContactos();
